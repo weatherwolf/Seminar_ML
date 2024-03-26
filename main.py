db610cbae9c33997e815f3abe75e260de40738fa
Weatherwolf <wolfbekkers@gmail.com>
2024-03-26 23:02:24 +0100

    small Changes

--- main.py
+++ main.py
@@ -1,29 +1,32 @@
 # %%
-from src.TestData import CreateData
+from src.Functions import Functions
 import pandas as pd
 
 # %%
+"""
+select parameters which you want to use to run the model
+- beginTime -> begin of the initial time frame. This observation is taken into the timeframe
+- endTime -> end of the initial time frame. This observation is not taken into the timeframe
+
+Note: endTime - beginTime determines the size of the timeframe we want to take a look at.
+
+- name -> the name of the dataset we want to take a look at
+
+- max_iter -> set the maximum number of iterations for the methods 
+
+"""
+
 beginTime = pd.Timestamp('1990-01-01')
 endTime = pd.Timestamp('2000-01-01')
+name = '2015-07.csv'
 
 # %%
-name = '2015-07.csv'
 data = pd.read_csv(name)
 
 # %%
-if name == '2015-07.csv':
-    data = data.drop(data.index[0])
-
-data['sasdate'] = pd.to_datetime(data['sasdate'])
-data.set_index('sasdate', inplace=True)
-
-data_cleaned = data.dropna()
-data_cleaned.reset_index(inplace=True)
+createData = Functions(beginTime, endTime, data, name)
 
 # %%
-createData = CreateData(beginTime, endTime, data_cleaned)
-
-# %%
-error = createData.RollingWindow("RPI", 'Lasso')
-print(error)
+error_Lasso = createData.RollingWindow('RPI', 'Lasso')
+error_Ridge = createData.RollingWindow('RPI', 'Ridge')
 # %%
--- src/Functions.py
+++ src/Functions.py
@@ -2,13 +2,33 @@ import pandas as pd
 import numpy as np
 from sklearn import linear_model
 
-class CreateData:
+class Functions:
 
-    def __init__(self, beginTime, endTime, data):
+    def __init__(self, beginTime, endTime, data, name, max_iter=1000):
         self.beginTime = beginTime
         self.endTime = endTime
         self.data = data
-        
+        self.name = name
+        self.max_iter = max_iter
+
+        self.cleanData()
+
+    
+    def cleanData(self):
+        data = self.data
+
+        if self.name == '2015-07.csv':
+            data = data.drop(data.index[0])
+
+        if self.name == '2015-07.csv' or self.name == '2024-02.csv':
+            data['sasdate'] = pd.to_datetime(data['sasdate'])
+            data.set_index('sasdate', inplace=True)
+
+            data_cleaned = data.dropna()
+            data_cleaned.reset_index(inplace=True)
+
+            self.data = data_cleaned
+
 
     def CreateDataSets(self, dependentVariable):
 
@@ -26,8 +46,7 @@ class CreateData:
 
         return [x_train, y_train, x_test, y_test]
     
-
-    
+   
     def MSE(self, y, x, coef, intercept):
         y_bar = intercept
 
@@ -36,18 +55,18 @@ class CreateData:
 
         # print(f"y-y_bar = {y} - {y_bar}")
         return (y-y_bar)*(y-y_bar)
-    
 
-    
-    def RollingWindow(self, dependentVariable, l = 1, l1_ratio=0.5, method=None):
+     
+    def RollingWindow(self, dependentVariable, method = None, l = 1, l1_ratio=0.5):
         if method == "Lasso":
-            method = linear_model.Lasso(alpha = l, max_iter=10000)
+            method = linear_model.Lasso(alpha = l, max_iter=self.max_iter)
 
         elif method == "Ridge":
-            method = linear_model.Ridge(alpha= l)
+            print("Ridge")
+            method = linear_model.Ridge(alpha= l, max_iter=self.max_iter)
 
         elif method == "ElasticNet":
-            method == linear_model.ElasticNet(alpha=l, l1_ratio=l1_ratio)
+            method == linear_model.ElasticNet(alpha=l, l1_ratio=l1_ratio, max_iter=self.max_iter)
 
         else:
             method = None
@@ -55,8 +74,10 @@ class CreateData:
         numberOfWindows = 0
         totalError = 0
 
-        while(self.endTime + pd.DateOffset(months=1) < max(self.data['sasdate']) and method):
-            
+        beginTime = self.beginTime
+        endTime = self.endTime
+
+        while(endTime + pd.DateOffset(months=1) < max(self.data['sasdate']) and method):
                 numberOfWindows += 1
 
                 [x_train, y_train, x_test, y_test] = self.CreateDataSets(dependentVariable)
@@ -66,8 +87,10 @@ class CreateData:
 
                 totalError += self.MSE(y_test, x_test, coef, intercept)
 
-                self.endTime = self.endTime + pd.DateOffset(months=1)
-                self.beginTime = self.beginTime + pd.DateOffset(months=1)
+                endTime = endTime + pd.DateOffset(months=1)
+                beginTime = beginTime + pd.DateOffset(months=1)
+
+                print(f"totalError: {totalError}")
 
                 # print(f'totalError: {totalError}, numberofWindows: {numberOfWindows}')
     
@@ -78,3 +101,16 @@ class CreateData:
         return totalError/numberOfWindows
 
 
+    # def RandomSearch(self, dependentVariable, method, logLambdas):
+    #     error = 1000000
+    #     lam = None
+
+    #     for l in logLambdas:
+    #         newError = self.RollingWindow(dependentVariable, method)
+
+
+    #         if self.RollingWindow(dependentVariable, method) < error:
+    #             error = newError
+    #             lam = l
+        
+    
